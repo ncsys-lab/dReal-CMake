72c7cd966f245259a61fa2aeb30d2f6ee1736d80
Kunal Sheth <kunal@kunalsheth.info>
2025-04-10 16:40:36 -0700

    Implement predicate statistics collector for future heuristic development.

---
+++ src/dreal/util/predicate_heuristic.cc
@@ -0,0 +1,295 @@
+/*
+   Copyright 2017 Toyota Research Institute
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+#include "dreal/util/predicate_heuristic.h"
+
+#include <dreal/symbolic/symbolic_expression_cell.h>
+#include <dreal/symbolic/symbolic_formula_cell.h>
+
+#include "exception.h"
+#include "logging.h"
+
+namespace dreal
+{
+    PredicateHeuristic::predicate_stats_t PredicateHeuristic::collect_statistics(const Expression& e) {
+        const auto it = ecache.find(e);
+        if (it == ecache.cend()) {
+            predicate_stats_t stats = {0};
+            recHeurExpr(e, stats);
+            stats.unique_variables = e.GetVariables().size();
+            ecache.try_emplace(e, stats);
+            return stats;
+        }
+        else return it->second;
+    }
+
+    PredicateHeuristic::predicate_stats_t PredicateHeuristic::collect_statistics(const Formula& f, const bool inverted) {
+        auto& fcache = inverted ? fcache_neg : fcache_pos;
+        const auto it = fcache.find(f);
+        if (it == fcache.cend()) {
+            predicate_stats_t stats = {0};
+            recHeurForm(f, stats, inverted);
+            stats.unique_variables = f.GetFreeVariables().size();
+            if (
+                !(is_negation(f) && is_nary(get_operand(f))) &&
+                !is_nary(f) // these will be cached at a lower level
+            ) fcache.try_emplace(f, stats);
+            return stats;
+        }
+        else return it->second;
+    }
+
+#define ADD_DECL(name) void PredicateHeuristic::name (const Expression &e, predicate_stats_t &stats) const
+    ADD_DECL(VisitVariable) {
+        stats.variable_cntr++;
+    }
+
+    ADD_DECL(VisitConstant) {
+        stats.constant_cntr++;
+    }
+
+    ADD_DECL(VisitRealConstant) {
+        stats.realconstant_cntr++;
+    }
+
+    ADD_DECL(VisitAddition) {
+        const auto& map = get_expr_to_coeff_map_in_addition(e);
+        stats.addition_cntr += map.size();
+        for (const auto& [expr, coeff] : map) {
+            recHeurExpr(expr, stats);
+        }
+    }
+
+    ADD_DECL(VisitMultiplication) {
+        const auto map = get_base_to_exponent_map_in_multiplication(e);
+        stats.multiplication_cntr += map.size();
+        for (const auto& [base, exp] : map) {
+            recHeurExpr(base, stats);
+            recHeurExpr(exp, stats);
+        }
+    }
+
+    ADD_DECL(VisitDivision) {
+        stats.division_cntr++;
+        BinaryOpHelper(e, stats);
+    }
+
+    ADD_DECL(VisitLog) {
+        stats.log_cntr++;
+        UnaryOpHelper(e, stats);
+    }
+
+    ADD_DECL(VisitAbs) {
+        stats.abs_cntr++;
+        UnaryOpHelper(e, stats);
+    }
+
+    ADD_DECL(VisitExp) {
+        stats.exp_cntr++;
+        UnaryOpHelper(e, stats);
+    }
+
+    ADD_DECL(VisitSqrt) {
+        stats.sqrt_cntr++;
+        UnaryOpHelper(e, stats);
+    }
+
+    ADD_DECL(VisitPow) {
+        stats.pow_cntr++;
+        BinaryOpHelper(e, stats);
+    }
+
+    ADD_DECL(VisitSin) {
+        stats.trigonometry_cntr++;
+        UnaryOpHelper(e, stats);
+    }
+
+    ADD_DECL(VisitCos) {
+        stats.trigonometry_cntr++;
+        UnaryOpHelper(e, stats);
+    }
+
+    ADD_DECL(VisitTan) {
+        stats.trigonometry_cntr++;
+        UnaryOpHelper(e, stats);
+    }
+
+    ADD_DECL(VisitAsin) {
+        stats.trigonometry_cntr++;
+        UnaryOpHelper(e, stats);
+    }
+
+    ADD_DECL(VisitAcos) {
+        stats.trigonometry_cntr++;
+        UnaryOpHelper(e, stats);
+    }
+
+    ADD_DECL(VisitAtan) {
+        stats.trigonometry_cntr++;
+        UnaryOpHelper(e, stats);
+    }
+
+    ADD_DECL(VisitAtan2) {
+        stats.trigonometry_cntr++;
+        BinaryOpHelper(e, stats);
+    }
+
+    ADD_DECL(VisitSinh) {
+        stats.trigonometry_cntr++;
+        UnaryOpHelper(e, stats);
+    }
+
+    ADD_DECL(VisitCosh) {
+        stats.trigonometry_cntr++;
+        UnaryOpHelper(e, stats);
+    }
+
+    ADD_DECL(VisitTanh) {
+        stats.trigonometry_cntr++;
+        UnaryOpHelper(e, stats);
+    }
+
+    ADD_DECL(VisitMin) {
+        stats.min_cntr++;
+        UnaryOpHelper(e, stats);
+    }
+
+    ADD_DECL(VisitMax) {
+        stats.max_cntr++;
+        UnaryOpHelper(e, stats);
+    }
+
+    ADD_DECL(VisitIfThenElse) {
+        stats.ifthenelse_cntr++;
+        const auto ite = to_if_then_else(e);
+        recHeurForm(ite->get_conditional_formula(), stats, false);
+        recHeurExpr(ite->get_then_expression(), stats);
+        recHeurExpr(ite->get_else_expression(), stats);
+    }
+
+    ADD_DECL(VisitUninterpretedFunction) {
+        stats.uninterpretedfunction_cntr++;
+    }
+
+#undef ADD_DECL
+#define ADD_DECL(name) void PredicateHeuristic::name (const Formula &f, predicate_stats_t &stats, const bool &inverted) const
+    ADD_DECL(VisitFalse) {
+        stats.false_cntr++;
+    }
+
+    ADD_DECL(VisitTrue) {
+        stats.true_cntr++;
+    }
+
+    ADD_DECL(VisitVariable) {
+        stats.variable_cntr++;
+    }
+
+    ADD_DECL(VisitEqualTo) {
+        stats.equalto_cntr++;
+        BinaryOpHelper(f, stats, inverted);
+    }
+
+    ADD_DECL(VisitNotEqualTo) {
+        stats.notequalto_cntr++;
+        BinaryOpHelper(f, stats, inverted);
+    }
+
+    ADD_DECL(VisitGreaterThan) {
+        stats.inequality_cntr++;
+        BinaryOpHelper(f, stats, inverted);
+    }
+
+    ADD_DECL(VisitGreaterThanOrEqualTo) {
+        stats.inequality_cntr++;
+        BinaryOpHelper(f, stats, inverted);
+    }
+
+    ADD_DECL(VisitLessThan) {
+        stats.inequality_cntr++;
+        BinaryOpHelper(f, stats, inverted);
+    }
+
+    ADD_DECL(VisitLessThanOrEqualTo) {
+        stats.inequality_cntr++;
+        BinaryOpHelper(f, stats, inverted);
+    }
+
+    ADD_DECL(VisitConjunction) {
+        const auto& ops = get_operands(f);
+        stats.conjunction_cntr += ops.size();
+        for (const auto& op : ops) recHeurFormCheckCache(op, stats, inverted);
+    }
+
+    ADD_DECL(VisitDisjunction) {
+        const auto& ops = get_operands(f);
+        stats.disjunction_cntr += ops.size();
+        for (const auto& op : ops) recHeurFormCheckCache(op, stats, inverted);
+    }
+
+    ADD_DECL(VisitNegation) {
+        stats.negation_cntr++;
+        UnaryOpHelper(f, stats, !inverted);
+    }
+
+    ADD_DECL(VisitForall) {
+        stats.forall_cntr++;
+        throw DREAL_RUNTIME_ERROR("Heuristics for quantifiers are currently unsupported {}", f);
+    }
+
+#undef ADD_DECL
+
+    void PredicateHeuristic::recHeurExprCheckCache(const Expression& e, predicate_stats_t& outer_stats) const {
+        const auto it = ecache.find(e);
+        if (it == ecache.cend()) recHeurExpr(e, outer_stats);
+        else outer_stats += it->second;
+    }
+
+    void PredicateHeuristic::recHeurExpr(const Expression& e, predicate_stats_t& stats) const {
+        return VisitExpression<void>(this, e, stats);
+    }
+
+    void PredicateHeuristic::recHeurFormCheckCache(const Formula& f, predicate_stats_t& outer_stats,
+                                                const bool& inverted) const {
+        auto& fcache = inverted ? fcache_neg : fcache_pos;
+        const auto it = fcache.find(f);
+        if (it == fcache.cend()) recHeurForm(f, outer_stats, inverted);
+        else outer_stats += it->second;
+    }
+
+    void PredicateHeuristic::recHeurForm(const Formula& f, predicate_stats_t& stats, const bool& inverted) const {
+        return VisitFormula<void>(this, f, stats, inverted);
+    }
+
+    void PredicateHeuristic::UnaryOpHelper(const Expression& e, predicate_stats_t& stats) const {
+        return recHeurExpr(get_argument(e), stats);
+    }
+
+    void PredicateHeuristic::UnaryOpHelper(const Formula& f, predicate_stats_t& stats, const bool& inverted) const {
+        return recHeurForm(get_operand(f), stats, inverted);
+    }
+
+    void PredicateHeuristic::BinaryOpHelper(const Expression& e, predicate_stats_t& stats) const {
+        const auto b = to_binary(e);
+        recHeurExpr(b->get_first_argument(), stats);
+        recHeurExpr(b->get_second_argument(), stats);
+    }
+
+    void PredicateHeuristic::BinaryOpHelper(const Formula& f, predicate_stats_t& stats, const bool& inverted) const {
+        const auto b = to_relational(f);
+        recHeurExpr(b->get_lhs_expression(), stats);
+        recHeurExpr(b->get_rhs_expression(), stats);
+    }
+} // namespace dreal
---
+++ src/dreal/util/predicate_heuristic.h
@@ -0,0 +1,225 @@
+//
+// Created by Kunal Sheth on 2/19/25.
+//
+
+#ifndef predicate_heuristic_H
+#define predicate_heuristic_H
+
+#include "dreal/symbolic/symbolic.h"
+#include "dreal/util/box.h"
+
+namespace dreal
+{
+    class PredicateHeuristic
+    {
+    // todo: unit test this!!!
+
+    public:
+        typedef struct
+        {
+            double raw_score;
+            unsigned unique_variables;
+            unsigned variable_cntr;
+            unsigned constant_cntr;
+            unsigned realconstant_cntr;
+            unsigned addition_cntr;
+            unsigned multiplication_cntr;
+            unsigned division_cntr;
+            unsigned log_cntr;
+            unsigned abs_cntr;
+            unsigned exp_cntr;
+            unsigned sqrt_cntr;
+            unsigned pow_cntr;
+            unsigned trigonometry_cntr;
+            unsigned min_cntr;
+            unsigned max_cntr;
+            unsigned ifthenelse_cntr;
+            unsigned uninterpretedfunction_cntr;
+            unsigned false_cntr;
+            unsigned true_cntr;
+            unsigned equalto_cntr;
+            unsigned notequalto_cntr;
+            unsigned inequality_cntr;
+            unsigned conjunction_cntr;
+            unsigned disjunction_cntr;
+            unsigned negation_cntr;
+            unsigned forall_cntr;
+        } predicate_stats_t;
+
+        static std::string predicate_stats_csv_header(const std::string& prefix) {
+            std::ostringstream s;
+            s << prefix << "raw_score,";
+            s << prefix << "unique_variables,";
+            s << prefix << "variable_cntr,";
+            s << prefix << "constant_cntr,";
+            s << prefix << "realconstant_cntr,";
+            s << prefix << "addition_cntr,";
+            s << prefix << "multiplication_cntr,";
+            s << prefix << "division_cntr,";
+            s << prefix << "log_cntr,";
+            s << prefix << "abs_cntr,";
+            s << prefix << "exp_cntr,";
+            s << prefix << "sqrt_cntr,";
+            s << prefix << "pow_cntr,";
+            s << prefix << "trigonometry_cntr,";
+            s << prefix << "min_cntr,";
+            s << prefix << "max_cntr,";
+            s << prefix << "ifthenelse_cntr,";
+            s << prefix << "uninterpretedfunction_cntr,";
+            s << prefix << "false_cntr,";
+            s << prefix << "true_cntr,";
+            s << prefix << "equalto_cntr,";
+            s << prefix << "notequalto_cntr,";
+            s << prefix << "inequality_cntr,";
+            s << prefix << "conjunction_cntr,";
+            s << prefix << "disjunction_cntr,";
+            s << prefix << "negation_cntr,";
+            s << prefix << "forall_cntr";
+            return s.str();
+        }
+
+        predicate_stats_t collect_statistics(const Expression& e);
+        predicate_stats_t collect_statistics(const Formula& f, bool inverted = false);
+
+    private:
+        // must be reference to const bool because of `drake::symbolic::VisitExpression...` template requires it.
+#define ADD_DECL(name) void name (const Expression &e, predicate_stats_t &stats) const
+        ADD_DECL(VisitVariable);
+        ADD_DECL(VisitConstant);
+        ADD_DECL(VisitRealConstant);
+        ADD_DECL(VisitAddition);
+        ADD_DECL(VisitMultiplication);
+        ADD_DECL(VisitDivision);
+        ADD_DECL(VisitLog);
+        ADD_DECL(VisitAbs);
+        ADD_DECL(VisitExp);
+        ADD_DECL(VisitSqrt);
+        ADD_DECL(VisitPow);
+        ADD_DECL(VisitSin);
+        ADD_DECL(VisitCos);
+        ADD_DECL(VisitTan);
+        ADD_DECL(VisitAsin);
+        ADD_DECL(VisitAcos);
+        ADD_DECL(VisitAtan);
+        ADD_DECL(VisitAtan2);
+        ADD_DECL(VisitSinh);
+        ADD_DECL(VisitCosh);
+        ADD_DECL(VisitTanh);
+        ADD_DECL(VisitMin);
+        ADD_DECL(VisitMax);
+        ADD_DECL(VisitIfThenElse);
+        ADD_DECL(VisitUninterpretedFunction);
+#undef ADD_DECL
+
+        // must be reference to const bool because of `drake::symbolic::VisitExpression...` template requires it.
+#define ADD_DECL(name) void name (const Formula &e, predicate_stats_t &stats, const bool &inverted) const
+        ADD_DECL(VisitFalse);
+        ADD_DECL(VisitTrue);
+        ADD_DECL(VisitVariable);
+        ADD_DECL(VisitEqualTo);
+        ADD_DECL(VisitNotEqualTo);
+        ADD_DECL(VisitGreaterThan);
+        ADD_DECL(VisitGreaterThanOrEqualTo);
+        ADD_DECL(VisitLessThan);
+        ADD_DECL(VisitLessThanOrEqualTo);
+        ADD_DECL(VisitConjunction);
+        ADD_DECL(VisitDisjunction);
+        ADD_DECL(VisitNegation);
+        ADD_DECL(VisitForall);
+#undef VISIT_DECL
+#undef ADD_DECL
+#undef VISIT_AND_ADD_DECL
+
+        std::unordered_map<Formula, predicate_stats_t> fcache_pos;
+        std::unordered_map<Formula, predicate_stats_t> fcache_neg;
+        std::unordered_map<Expression, predicate_stats_t> ecache;
+
+        void recHeurExprCheckCache(const Expression& e, predicate_stats_t& stats) const;
+        void recHeurExpr(const Expression& e, predicate_stats_t& stats) const;
+
+        void recHeurFormCheckCache(const Formula& f, predicate_stats_t& stats, const bool& inverted) const;
+        void recHeurForm(const Formula& f, predicate_stats_t& stats, const bool& inverted) const;
+
+        void UnaryOpHelper(const Expression& e, predicate_stats_t& stats) const;
+        void UnaryOpHelper(const Formula& f, predicate_stats_t& stats, const bool& inverted) const;
+        void BinaryOpHelper(const Expression& e, predicate_stats_t& stats) const;
+        void BinaryOpHelper(const Formula& f, predicate_stats_t& stats, const bool& inverted) const;
+        void NaryOpHelper(const Formula& f, predicate_stats_t& stats, bool inverted) const;
+
+        friend void drake::symbolic::VisitExpression<void>(
+            const PredicateHeuristic*, const Expression&, predicate_stats_t&
+        );
+        friend void drake::symbolic::VisitFormula<void>(
+            const PredicateHeuristic*, const Formula&, predicate_stats_t&, const bool&
+        );
+    };
+
+    inline std::ostream& operator<<(std::ostream& os, const PredicateHeuristic::predicate_stats_t& stats) {
+        os << stats.raw_score << ',';
+        os << stats.unique_variables << ',';
+        os << stats.variable_cntr << ',';
+        os << stats.constant_cntr << ',';
+        os << stats.realconstant_cntr << ',';
+        os << stats.addition_cntr << ',';
+        os << stats.multiplication_cntr << ',';
+        os << stats.division_cntr << ',';
+        os << stats.log_cntr << ',';
+        os << stats.abs_cntr << ',';
+        os << stats.exp_cntr << ',';
+        os << stats.sqrt_cntr << ',';
+        os << stats.pow_cntr << ',';
+        os << stats.trigonometry_cntr << ',';
+        os << stats.min_cntr << ',';
+        os << stats.max_cntr << ',';
+        os << stats.ifthenelse_cntr << ',';
+        os << stats.uninterpretedfunction_cntr << ',';
+        os << stats.false_cntr << ',';
+        os << stats.true_cntr << ',';
+        os << stats.equalto_cntr << ',';
+        os << stats.notequalto_cntr << ',';
+        os << stats.inequality_cntr << ',';
+        os << stats.conjunction_cntr << ',';
+        os << stats.disjunction_cntr << ',';
+        os << stats.negation_cntr << ',';
+        os << stats.forall_cntr;
+        return os;
+    }
+
+    inline PredicateHeuristic::predicate_stats_t& operator+=(
+        PredicateHeuristic::predicate_stats_t& lhs,
+        const PredicateHeuristic::predicate_stats_t& rhs
+    ) {
+        lhs.raw_score += rhs.raw_score;
+        lhs.unique_variables += rhs.unique_variables;
+        lhs.variable_cntr += rhs.variable_cntr;
+        lhs.constant_cntr += rhs.constant_cntr;
+        lhs.realconstant_cntr += rhs.realconstant_cntr;
+        lhs.addition_cntr += rhs.addition_cntr;
+        lhs.multiplication_cntr += rhs.multiplication_cntr;
+        lhs.division_cntr += rhs.division_cntr;
+        lhs.log_cntr += rhs.log_cntr;
+        lhs.abs_cntr += rhs.abs_cntr;
+        lhs.exp_cntr += rhs.exp_cntr;
+        lhs.sqrt_cntr += rhs.sqrt_cntr;
+        lhs.pow_cntr += rhs.pow_cntr;
+        lhs.trigonometry_cntr += rhs.trigonometry_cntr;
+        lhs.min_cntr += rhs.min_cntr;
+        lhs.max_cntr += rhs.max_cntr;
+        lhs.ifthenelse_cntr += rhs.ifthenelse_cntr;
+        lhs.uninterpretedfunction_cntr += rhs.uninterpretedfunction_cntr;
+        lhs.false_cntr += rhs.false_cntr;
+        lhs.true_cntr += rhs.true_cntr;
+        lhs.equalto_cntr += rhs.equalto_cntr;
+        lhs.notequalto_cntr += rhs.notequalto_cntr;
+        lhs.inequality_cntr += rhs.inequality_cntr;
+        lhs.conjunction_cntr += rhs.conjunction_cntr;
+        lhs.disjunction_cntr += rhs.disjunction_cntr;
+        lhs.negation_cntr += rhs.negation_cntr;
+        lhs.forall_cntr += rhs.forall_cntr;
+        return lhs;
+    }
+} // namespace dreal
+
+// * learned regression model here *
+
+#endif //predicate_heuristic_H
