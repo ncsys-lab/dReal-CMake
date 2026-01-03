3e05991f7c0a519c279ae1f28a02cb99aadd5c0a
Kunal Sheth <kunal@kunalsheth.info>
2026-01-02 19:24:49 -0800

    Switch CAV26 parser's `t` field from double to optional<int>.

--- src/dreal/util/pattern_matching/CAV26_varname_parser.h
+++ src/dreal/util/pattern_matching/CAV26_varname_parser.h
@@ -24,9 +24,9 @@ namespace dreal
     static const std::regex SAR_VARIABLE_RE{R"(^(.+)_t(\d+)(ad|bc)$)"};
     static const std::regex DRH_VARIABLE_RE{R"(^(.+)_(\d+)(_t|_0)?$)"};
 
-    static auto CAV26_SAR_PARSER(const std::string& name) -> std::pair<std::string, double> {
+    static std::pair<std::string, std::optional<int>> const& CAV26_SAR_PARSER(const std::string& name) {
         std::string prefix{};
-        double t = -1; // "unknown"
+        std::optional<int> t{};
 
         if (std::smatch m; std::regex_search(name, m, SAR_VARIABLE_RE)) {
             const auto mag = std::stoi(m[2].str());
@@ -35,23 +35,22 @@ namespace dreal
             t = mag * (polarity == "bc" ? -1 : +1);
         }
         else if (name.rfind("ITE", 0) == 0) { // known issue...
-            const auto last = name.rfind("ITE");
             prefix = name; // "ITE";
-            // t is unknown ... // if (last != std::string::npos) t = std::stoi(name.substr(last + 3));
+            t = std::nullopt; // const auto last = name.rfind("ITE"); if (last != std::string::npos) t = std::stoi(name.substr(last + 3));
         }
         else throw DREAL_RUNTIME_ERROR("Invalid SAR variable name: {}", name);
 
         return {std::move(prefix), t};
     }
 
-    static auto CAV26_DRH_PARSER(const std::string& name) -> std::pair<std::string, double> {
+    static auto CAV26_DRH_PARSER(const std::string& name) -> std::pair<std::string, std::optional<int>> {
         if (std::smatch m; std::regex_search(name, m, DRH_VARIABLE_RE)) {
-            const auto mag = std::stoi(m[2].str());
+            const int mag = std::stoi(m[2].str());
             const auto step = m[3].matched ? m[3].str() : std::string{};
 
             auto prefix = m[1].str();
-            const auto t = mag + (step == "_t" ? 0.5 : 0.0);
             return {std::move(prefix), t};
+            const int t = (2 * mag) + (step == "_t" ? 1 : 0);
         }
         throw DREAL_RUNTIME_ERROR("Invalid DRH variable name: {}", name);
     }
